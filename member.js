912669f1f97907d3ab1e923382c7f43d2860e48a
René Kraus <83776297+SONNYXT@users.noreply.github.com>
2023-07-29 21:25:23 +0200

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,12 @@
-function skillsmember() {
-    return {
-        restrict: 'E',
-        templateUrl: 'templates/skills.html',
-        controller: 'SkillsController',
-        controllerAs: 'vm',
-        bindToController: true,
-        scope: {
-            member: '='
-        }
-    };
+function skillsMember() {
+    var skills = document.getElementById("skills").value;
+    var skills = skills.split(",");
+    var skills = skills.map(function (item) {
+        return item.trim();
+    });
+    var skills = skills.filter(function (item) {
+        return item != "";
+    });
+    var skills = skills.sort();
+    document.getElementById("skills").value = skills.join(", ");
 }
